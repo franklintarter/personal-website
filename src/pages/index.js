0050c8cf7206a6d20dc364408a4ca0a365b89946
Franklin Tarter <franklintarter@gmail.com>
2020-04-16 16:42:53 -0700

    maybe it's what I like - wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import { useArticles } from "../hooks";
 import ProjectPreview from "../components/UI/ProjectPreview";
 import ArticlePreview from "../components/UI/ArticlePreview";
 import { A } from "../components/Typography";
+import ChevronRight from "../img/svg/ui/chevron-right.svg";
 
 const pageTitle = "Home";
 
@@ -62,22 +63,25 @@ export default () => {
       </h1>
       <div className="mt-16">
         {/* <h2 className="text-2xl text-brand-gray uppercase mb-4">Writing</h2> */}
-        {/* <h2 className="text-lg uppercase text-brand-gray font-serif mb-3 font-light">
+        <h2 className="text-lg uppercase text-brand-gray font-serif mb-3 font-light">
           ~ Writing ~
-        </h2> */}
-        <div className="mb-8">{articleEls}</div>
-        <A
-          to="/writing"
-          className="text-lg py-4 text-gray-800"
-          // className="underline text-gray-800 hover:text-brand-faded"
-        >
-          More writing..
-        </A>
+        </h2>
+        <div className="mb-4">{articleEls}</div>
+        <div className="flex items-end">
+          <A
+            to="/writing"
+            className="text-lg py-4 text-gray-700 text-right ml-auto block flex align-center"
+            // className="underline text-gray-800 hover:text-brand-faded"
+          >
+            <span className="mr-3">More writing</span>
+            <ChevronRight className="w-2 fill-current" />
+          </A>
+        </div>
       </div>
       <div className="mt-16">
-        {/* <h2 className="text-lg uppercase text-brand-gray font-serif mb-3 font-light">
+        <h2 className="text-lg uppercase text-brand-gray font-serif mb-3 font-light">
           ~ Other ~
-        </h2> */}
+        </h2>
         <div>
           <ProjectPreview
             url="/bubble-sort"
